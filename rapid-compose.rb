951a23f9ee2c5638484ab3d33c35663c31dc73bf
goreleaserbot <goreleaser@carlosbecker.com>
2019-04-17 21:58:57 +0200

    Brew formula update for rapid-compose version v1.4.0

--- rapid-compose.rb
+++ rapid-compose.rb
@@ -2,9 +2,9 @@
 class RapidCompose < Formula
   desc "A teminal UI to start individual services from docker-copose.yaml"
   homepage ""
-  url "https://github.com/trusz/rapid-compose/releases/download/v1.3.1/rapid-compose_1.3.1_Darwin_x86_64.tar.gz"
-  version "1.3.1"
-  sha256 "cbf7eb3458207ada30d65ea069e8aa8b5c1dbb583b447565728ae2a3c9993fb1"
+  url "https://github.com/trusz/rapid-compose/releases/download/v1.4.0/rapid-compose_1.4.0_Darwin_x86_64.tar.gz"
+  version "1.4.0"
+  sha256 "fd29c78b1832a228083c098817fd74ea32dcd2a9485dfa8883e4cfe6194a3283"
 
   def install
     bin.install "rc"
